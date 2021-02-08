415436db2fd04fb01d21bf299be839c7f8f6e0fd
Tami Bar <fire17@gmail.com>
2021-02-08 20:12:52 +0200

    resume update

--- _includes/js/search-worker.js
+++ _includes/js/search-worker.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2020 Florian Klampfer <https://qwtel.com/>
+// Copyright (c) 2021 Florian Klampfer <https://qwtel.com/>
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
